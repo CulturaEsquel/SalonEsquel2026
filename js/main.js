aa9ca33629e03e5793f3493ddfe57b81fb56c0dc
CulturaEsquel <infoculturalesquel@gmail.com>
2026-09-25 19:26:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Cargamos ambos JSON en paralelo
     Promise.all([
         fetch('data/premios-especiales.json').then(res => res.json()),
-        fetch('data/secciones.json').then(res => res.json())
+        fetch('data/selecciones.json').then(res => res.json())
     ])
     .then(([premiosData, seccionesData]) => {
         
